5ae53c2cb89ffb410d51ccfc0ec3a74dc61595ae
Koren42 <nacho242004@hotmail.com>
2026-03-30 15:03:13 -0600

    "Login inicial, faltan cambios"

--- SC-403-ProyectoFinalGrupo4/src/main/java/tienda/proyecto/SecurityConfig.java
+++ SC-403-ProyectoFinalGrupo4/src/main/java/tienda/proyecto/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -70,5 +71,10 @@ public class SecurityConfig {
             @Lazy UserDetailsService userDetailsService) throws Exception {
         build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
-
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
 }
